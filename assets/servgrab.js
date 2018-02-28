be6022664ec1f2659ba0c995f47da3dc7d4c59bd
ThatOneTqnk <chattanoogamuckers@gmail.com>
2018-02-28 15:37:14 -0600

    fixed none

--- assets/servgrab.js
+++ assets/servgrab.js
@@ -37,8 +37,12 @@ $(document).ready(function(){
             txtb.reverse();
             console.log(txtb);
             for(var y = 0; y < info.servers.length; y++) {
+                console.log(txtb[y].players.length);
+                if(txtb[y].players.length == 0) {
+                    var newstr = 'No players online!';
+                } else {
                 var newstr = txtb[y].players.join(", ");
-                console.log(newstr);
+                };
                 if(reprow==1) {
                     txtb[y]['shtm'] = '<div class="row"><div class="col s12 m2 offset-m1"><div class="card" style="width: 100%;"><div class="card-content black-text"><span class="card-title">' + txtb[y].sname + '</span><div id="myProgress"><div id="myBar" style="width:' + Math.ceil((100 * (txtb[y].nowp))/ txtb[y].maxp) + '%;"></div></div><p style="font-weight: 5;">' + txtb[y].nowp + '/' + txtb[y].maxp + ' online. </p></div><div class="card-action"><a href="#modal' + xcount + '" class="modal-trigger">More Info</a></div></div></div><div id="modal' + xcount + '" class="modal modal-fixed-footer"><div class="modal-content"><h4>Server Info</h4><br><p>Players online:</p><br><p>' + newstr + '</p></div><div class="modal-footer"><a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Close</a></div></div><span class="trans"></span>';
                 } else if(reprow==2) {
